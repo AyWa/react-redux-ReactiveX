71f90804db846e90e0c0077d1f572b45095bed72
Marc Hurabielle <marc.hurabielle@gmail.com>
2017-04-06 23:01:09 +0900

    refactor api

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,7 @@ import { ConnectedRouter } from 'react-router-redux'
 import { history } from './store'
 import NotFound from './components/errors/404';
 import Container from './components/container';
-import aa from './utilities/api';
+import aa from './api/config';
 
 export * from './app.scss';
 
