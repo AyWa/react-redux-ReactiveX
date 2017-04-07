0954002c5bf54e72e04b66f5256850bd7d6bc3c0
Marc Hurabielle <marc.hurabielle@gmail.com>
2017-04-07 13:35:24 +0900

    fix bookepic to take only dispatch last one

--- frontend/src/reducers-observable/book.js
+++ frontend/src/reducers-observable/book.js
@@ -1,7 +1,7 @@
 import {BOOK_SELECTED} from 'actions/types'
-import 'rxjs';
+import Rx from 'rxjs';
 
 export default action$ =>
   action$.ofType(BOOK_SELECTED)
-    .delay(5000)
-    .mapTo({ type: 'BOOK_RESET'})
+    .switchMap(_ => Rx.Observable.timer(3000)
+      .mapTo({ type: 'BOOK_RESET'}))
