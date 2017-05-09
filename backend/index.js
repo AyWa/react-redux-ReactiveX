8ac1ad19e6c089df76e13a3d3d7380c09c4a2024
Marc Hurabielle <marc.hurabielle@gmail.com>
2017-05-09 19:59:55 +0900

    add prerender

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const prerender = require('prerender-node')
 
 // could be use later to do some react render etc
 
@@ -9,6 +10,9 @@ const frontDirectory = 'frontend';
 const frontIndex = 'index.html';
 // Start the server
 app.listen(port);
+
+// set up our middleware for prerender app for crawler
+app.use(prerender);
 
 // Get our request parameters
 app.use(express.static(path.join(__dirname, frontDirectory)));
