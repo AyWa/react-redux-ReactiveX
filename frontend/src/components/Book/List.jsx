9125f19b72dbdaebbd9d3ae42d1df4aa1961c05d
Marc Hurabielle <marc.hurabielle@gmail.com>
2017-07-07 00:16:27 +0900

    start implement grid

--- frontend/src/components/Book/List.jsx
+++ frontend/src/components/Book/List.jsx
@@ -9,6 +9,7 @@ import Tag from 'reusable/UI/Tag'
 import Progress from 'reusable/UI/Progress'
 import Card from 'reusable/UI/Card'
 import Image from 'reusable/UI/Image'
+import Grid from "reusable/UI/Grid"
 import {TEST} from 'utilities/modals'
 import BookDetail from './Detail';
 
@@ -58,38 +59,43 @@ class BookList extends Component {
         <ul className="list-group col-md-4">
           {feed ? this.renderFeed() : ''}
         </ul>
-        <Button
-          title="loading"
-          color={color.warning}
-          isLoading
-        />
-        <Button
-          title="inverted disabled"
-          color={color.success}
-          isInverted
-          disabled
-        />
-        <Button
-          title="focused"
-          color={color.primary}
-          isFocused
-        />
-        <Button
-          title="test modal"
-          color={color.primary}
-          onClick={_ => setModal({
-            modal: TEST,
-          })}
-        />
-        <Button
-          title="test alert"
-          isOutlined
-          color={color.danger}
-          onClick={_ => setAlert({
-            body: 'this is a strong error',
-            modifier: color.danger,
-          })}
-        />
+        <Grid>
+          <div>
+            <Button
+              style={{width: "100%"}}
+              title="loading"
+              color={color.warning}
+              isLoading
+            />
+          </div>
+          <Button
+            title="inverted disabled"
+            color={color.success}
+            isInverted
+            disabled
+          />
+          <Button
+            title="focused"
+            color={color.primary}
+            isFocused
+          />
+          <Button
+            title="test modal"
+            color={color.primary}
+            onClick={_ => setModal({
+              modal: TEST,
+            })}
+          />
+          <Button
+            title="test alert"
+            isOutlined
+            color={color.danger}
+            onClick={_ => setAlert({
+              body: 'this is a strong error',
+              modifier: color.danger,
+            })}
+          />
+        </Grid>
         <Tag
           title="test click"
           color={color.danger}
@@ -110,34 +116,39 @@ class BookList extends Component {
           max={100}
           color={color.info}
         />
-        <Card
-          img={
-            <Image
-              size="4by3"
-              src="http://bulma.io/images/placeholders/1280x960.png"
-            />
-          }
-          initialState
-          headerTitle="coucou"
-          footer={footer}
-        >
+        <Grid>
           <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-            <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
-            <br />
-            <small>11:09 PM - 1 Jan 2016</small>
+            <Card
+              img={
+                <Image
+                  size="4by3"
+                  src="http://bulma.io/images/placeholders/1280x960.png"
+                />
+              }
+              initialState
+              headerTitle="coucou"
+              footer={footer}
+            >
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br />
+                <small>11:09 PM - 1 Jan 2016</small>
+              </div>
+            </Card>
           </div>
-        </Card>
-        <br />
-        ---limit --
-        <Card>
           <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-            <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
-            <br />
-            <small>11:09 PM - 1 Jan 2016</small>
+            <Card>
+              <div>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
+                <a>@bulmaio</a>. <a>#css</a> <a>#responsive</a>
+                <br />
+                <small>11:09 PM - 1 Jan 2016</small>
+              </div>
+            </Card>
           </div>
-        </Card>
+        </Grid>
+
       </div>
     )
   }
---
+++ frontend/src/reusable/UI/Grid.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import "./grid.scss"
+
+const Column = ({children}) => {
+  return React.cloneElement(children, {
+    ...children.props,
+    className: `${children.props.className} column`,
+  })
+}
+export default ({children}) => {
+  return (
+    <div className="columns">
+      {
+        children.map(c => <Column>{c}</Column>)
+      }
+    </div>
+  )
+}
